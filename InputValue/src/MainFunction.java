d6e2ca0a4323b793f60ee8ce8ba13205805c8601
Meraj Ali <meraj09034@gmail.com>
2016-02-24 10:13:53 +0600

    fgf
    gf

---
+++ InputValue/src/MainFunction.java
@@ -0,0 +1,23 @@
+
+ public class Person {
+
+	
+	int a=10;
+	public void add()
+	{
+		System.out.print(a);
+	}
+}
+
+public class MainFunction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Person ob=new Person();
+		ob.add();
+
+	}
+
+}
